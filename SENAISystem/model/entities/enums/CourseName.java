c616b06d05defc017c80a999f4560995b53ab2ec
Arthur Freitas <freitarthur176@gmail.com>
2026-03-11 10:32:51 -0300

    Create CourseName.java

---
+++ SENAISystem/model/entities/enums/CourseName.java
@@ -0,0 +1,12 @@
+package SENAISystem.model.entities.enums;
+
+public enum CourseName {
+    MECÂNICA,
+    ELÉTRICA,
+    ELETRÔNICA,
+    TI,
+    EDIFICAÇÕES,
+    MARCENARIA,
+    DESENHO_MECÂNICO,
+    CONFEITARIA;
+}
